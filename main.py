82353b9734e22fa8915b7a4a0c1d603b752f1bca
jessieitc <jnyarko@itconsortiumgh.com>
2025-11-21 21:14:21 +0000

    audio plays in tts now

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from ellabs import elevenlabs_tts, elevenlabs_stt
 from mangum import Mangum
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, StreamingResponse
 import base64
 import os
+import io
 from dotenv import load_dotenv
 from elevenlabs import ElevenLabs
 
@@ -25,12 +26,10 @@ class Model(BaseModel):
 
 @app.post("/text-to-speech")
 async def choose(payload: Model):
-    if payload.text.strip():
-        result = elevenlabs_tts(payload.text)
-        audio_base64 = base64.b64encode(result).decode("utf-8")
-        return JSONResponse(content={"audio_base64": audio_base64})
+    if payload.text != "":
+        return elevenlabs_tts(payload.text)
     else:
-        return JSONResponse(content={"error": "Text must be added."})
+        return {"error": "Text must be added."}
 
 @app.post("/speech-to-text")
 async def select(payload: Model):
